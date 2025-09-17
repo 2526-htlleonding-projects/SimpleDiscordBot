cbf3731689ccf21c0e53859c174b70678fe0b228
arthur <a.dirnberger@htl-leonding.ac.at>
2025-09-17 16:08:27 +0200

    removing unecasary project in solution (stable)

--- src/DiscordBot_Core/modules/TheFunnyOne.cs
+++
@@ -1,8 +0,0 @@
-namespace DiscordBot_Core.modules;
-
-public class TheFunnyOne : ModuleBase<SocketCommandContext>
-{
-    [Command("funny")]
-    public async Task PingAsync()
-        => await ReplyAsync("Why don’t scientists trust atoms? Because they make up everything!");
-}
--- src/DiscordBot_Module/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DiscordBot_Core;
-
-static class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("This is the Module-Project!");
-    }
-}
